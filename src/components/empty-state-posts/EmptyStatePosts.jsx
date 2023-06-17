ab867c8bd26ebe52f2669c8941f7477e75b5e141
agustinusadam49 <agustinusadamw@gmail.com>
2023-06-17 13:30:08 +0700

    Improvement on wording empty state post component

--- src/components/empty-state-posts/EmptyStatePosts.jsx
+++ src/components/empty-state-posts/EmptyStatePosts.jsx
@@ -3,16 +3,29 @@ import "./EmptyStatePosts.scss";
 
 const EmptyStatePosts = ({ params }) => {
   const { username, location } = params;
+  const firstWording = "Belum ada Public Post";
+  const isHomePageLocation = location === "home-page";
+  const isMyProfilePageLocation = location === "profile-page-user-loggedin";
+  const tellUserToMakePost = `${
+    isMyProfilePageLocation ? "Buat" : isHomePageLocation ? "buat" : ""
+  } postingan anda dan click 'share'.`;
+  const posibilityFollowWording = `Silahkan follow ${
+    isHomePageLocation ? "users" : username
+  } untuk kemungkinan melihat postingan dengan status Followers Only.`;
+  const additionalHomePageWording = `${posibilityFollowWording} Atau ${tellUserToMakePost}`;
+  const homePageWording = `Anda dan user lainnya belum mem-publish postingan. ${additionalHomePageWording}`;
+  const profilePageUserLoggedinWording = tellUserToMakePost;
+  const profilePageOtherUserWording = `${username} belum membuat postingan. ${posibilityFollowWording}`;
 
   return (
     <div className="empty-state-posts-parrent">
-      <div className="first-wording">Belum ada postingan</div>
+      <div className="first-wording">{firstWording}</div>
       <div className="second-wording">
-        {location === "home-page"
-          ? "Anda dan user lainnya belum membuat postingan."
-          : location === "profile-page-user-loggedin"
-          ? "Silahkan buat postingan anda dan click 'share'."
-          : `${username} belum membuat postingan.`}
+        {isHomePageLocation
+          ? homePageWording
+          : isMyProfilePageLocation
+          ? profilePageUserLoggedinWording
+          : profilePageOtherUserWording}
       </div>
     </div>
   );
