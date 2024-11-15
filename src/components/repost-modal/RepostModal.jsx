6834bccaba16299ef4a068a73295af7d9fb8ab01
agustinusadam49 <agustinusadamw@gmail.com>
2024-11-15 13:58:01 +0700

    Refactor some codes in RepostModal component

--- src/components/repost-modal/RepostModal.jsx
+++ src/components/repost-modal/RepostModal.jsx
@@ -1,47 +1,37 @@
-import React, {
-  useReducer,
-  useState,
-  useMemo,
-  Fragment,
-  useCallback,
-  useEffect,
-} from "react";
+import React, { useReducer, useState, useMemo, Fragment } from "react";
 import CancelIcon from "@mui/icons-material/Cancel";
 import {
   setIsRepostModalOpen,
   setPostItem,
   setIsAddPosting,
-  setOpenLoadDataModal,
 } from "../../redux/slices/postsSlice";
 import { setIsClicked } from "../../redux/slices/buttonsSlice";
 import GlobalButton from "../button/GlobalButton";
 import RepostOptionStatus from "../option-status/RepostOptionStatus";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  INITIAL_SHARE_STATE,
-  shareActionType,
-  shareReducer,
-} from "../share/share-reducer";
 import InputTextGlobal from "../../components/input-text-global/InputTextGlobal";
 import { useFormValidation } from "../../custom-hooks/useFormValidation";
 import Post from "../posting/Post";
-import { useShareCommonHooks } from "../share/useShareCommonHooks";
-import { useShareLoadItems } from "../share/useShareLoadItems";
 import { createNewRepost } from "../../apiCalls/postsApiFetch";
+import {
+  INITIAL_LOADING_STATE,
+  actionType,
+  loadingReducer,
+} from "../../utils/reducers/globalLoadingReducer";
 
 import "./RepostModal.scss";
 
-export default function RepostModal() {
-  const intervalToClosePostLoadModal = 4000;
-  const [shareState, mutate] = useReducer(shareReducer, INITIAL_SHARE_STATE);
+const intervalToCloseRepostModal = 4000;
 
+export default function RepostModal() {
+  const [loadingState, mutate] = useReducer(
+    loadingReducer,
+    INITIAL_LOADING_STATE
+  );
   const dispatch = useDispatch();
 
   const currentUserNameFromSlice = useSelector((state) => state.user.userName);
   const postItemFromSlice = useSelector((state) => state.posts.postItem);
-  const openLoadDataModalSlice = useSelector(
-    ({ posts }) => posts.openLoadDataModal
-  );
   const [activeRepostType, setActiveRepostType] = useState("REPOST");
   const [caption, setCaption] = useState("");
 
@@ -69,52 +59,31 @@ export default function RepostModal() {
   const closeRepostModalEdit = (value) => {
     dispatch(setIsRepostModalOpen({ isRepostModalOpen: value }));
     dispatch(setPostItem({ postItem: null }));
-    setActiveRepostType("REPOST");
   };
 
-  const { runPostPendingThenStopIt } = useShareCommonHooks({
-    onRunPostPendingThenStopIt: () => {
-      mutate({ type: shareActionType.RUN_POST_PENDING });
-      setTimeout(() => {
-        mutate({ type: shareActionType.STOP_POST_PENDING });
-      }, 2000);
-    },
-  });
+  const hitCreateNewRepostApi = (payloadBodyObj) => {
+    mutate({ type: actionType.RUN_LOADING_STATUS });
 
-  const hitCreateNewRepostApi = useCallback(
-    async (payloadBodyObj) => {
-      await runPostPendingThenStopIt();
-      mutate({ type: shareActionType.RUN_POST_LOADING });
-      createNewRepost(payloadBodyObj)
-        .then((postingResult) => {
-          if (postingResult.data.success) {
-            mutate({ type: shareActionType.STOP_POST_LOADING_WHEN_SUCCESS });
-            dispatch(setIsAddPosting({ isSuccessPosting: true }));
-
-            setTimeout(() => {
-              dispatch(setOpenLoadDataModal({ payload: false }));
-              dispatch(setIsRepostModalOpen({ isRepostModalOpen: false }));
-              dispatch(setPostItem({ postItem: null }));
-              setActiveRepostType("REPOST");
-            }, intervalToClosePostLoadModal);
-          }
-        })
-        .catch((error) => {
-          setCaption("");
-          mutate({ type: shareActionType.STOP_POST_LOADING });
-          const errorMessageFromServer = error.response.data.errorMessage;
-          console.error("errorMessageFromServer", errorMessageFromServer);
+    createNewRepost(payloadBodyObj)
+      .then((postingResult) => {
+        if (postingResult.data.success) {
+          dispatch(setIsAddPosting({ isSuccessPosting: true }));
 
           setTimeout(() => {
-            dispatch(setOpenLoadDataModal({ payload: false }));
-          }, intervalToClosePostLoadModal);
-        });
-    },
-    [dispatch, runPostPendingThenStopIt]
-  );
+            mutate({ type: actionType.STOP_LOADING_STATUS });
+            closeRepostModalEdit(false);
+          }, intervalToCloseRepostModal);
+        }
+      })
+      .catch((error) => {
+        console.error("errorMessageFromServer", error);
+        setCaption("");
+        mutate({ type: actionType.STOP_LOADING_STATUS });
+      });
+  };
 
-  const doSaveRepost = useCallback(async () => {
-    dispatch(setOpenLoadDataModal({ payload: true }));
+  const doSaveRepost = () => {
+    if (loadingState.status) return;
 
     const repostPayload = {
       postCaption: caption,
@@ -132,32 +101,7 @@ export default function RepostModal() {
         hitCreateNewRepostApi(repostPayload);
       }
     }
-  }, [
-    activeRepostType,
-    caption,
-    currentUserNameFromSlice,
-    dispatch,
-    hitCreateNewRepostApi,
-    isValid,
-    postItemFromSlice.id,
-  ]);
-
-  useShareLoadItems({
-    inputCaption: caption || "reposted",
-    inputFileImage: null,
-    inputShareState: shareState,
-    title: {
-      _1: "Upload Caption",
-      _2: "Upload Image",
-    },
-  });
-
-  useEffect(() => {
-    if (!openLoadDataModalSlice) {
-      mutate({ type: shareActionType.STOP_POST_AND_IMAGE_SUCCESS });
-      setCaption("");
-    }
-  }, [openLoadDataModalSlice]);
+  };
 
   return (
     <div className="content-container repost-modal">
@@ -165,10 +109,12 @@ export default function RepostModal() {
         {/* Top Content of Modal */}
         <div className="repost-modal-top-content">
           <div className="repost-modal-top-content-title">Repost Modal</div>
-          <CancelIcon
-            className="repost-modal-close-button"
-            onClick={() => closeRepostModalEdit(false)}
-          />
+          {!loadingState.status && (
+            <CancelIcon
+              className="repost-modal-close-button"
+              onClick={() => closeRepostModalEdit(false)}
+            />
+          )}
         </div>
 
         {/* Bottom Content of Modal */}
@@ -199,14 +145,20 @@ export default function RepostModal() {
           )}
 
           <div className="button-repost-wrapper">
-            <GlobalButton
-              buttonLabel={"Batal"}
-              classStyleName={"repost-button cancel"}
-              onClick={() => closeRepostModalEdit(false)}
-            />
+            {!loadingState.status && (
+              <GlobalButton
+                buttonLabel={"Batal"}
+                classStyleName={"repost-button cancel"}
+                onClick={() => closeRepostModalEdit(false)}
+              />
+            )}
 
             <GlobalButton
-              buttonLabel={getStatus(activeRepostType)}
+              buttonLabel={
+                loadingState.status
+                  ? "Loading ..."
+                  : getStatus(activeRepostType)
+              }
               classStyleName={"repost-button save"}
               onClick={doSaveRepost}
             />
