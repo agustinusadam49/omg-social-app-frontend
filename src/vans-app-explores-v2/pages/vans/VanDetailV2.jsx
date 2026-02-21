ceafa9be13677598faa0cfbc60e305dfb16a9731
agustinusadam49 <agustinusadamw@gmail.com>
2026-02-21 08:54:06 +0700

    van-app-v2: implement defer, Await and suspense component, also loading component in van detail page

--- src/vans-app-explores-v2/pages/vans/VanDetailV2.jsx
+++ src/vans-app-explores-v2/pages/vans/VanDetailV2.jsx
@@ -1,58 +1,74 @@
-import { Link, useLocation, useLoaderData } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
+import { Suspense } from "react";
 import { processGetVanDetailById } from "../../api-calls-simulations/api-calls";
 import { modifiedToClassCssName, authUserCheck } from "../../utils/index";
+import Loading from "../../components/Loading";
 
 export default function VanDetailV2() {
   const { state } = useLocation();
-  const vanDetail = useLoaderData();
+  const vanDetailPromise = useLoaderData();
 
-  const currentVanType = state?.type ?? "all";
-  const currentVanSearch = state?.search ?? "";
+  const renderVanDetailSection = (vanDetail) => {
+    const currentVanType = state?.type ?? "all";
+    const currentVanSearch = state?.search ?? "";
+
+    return (
+      <div className="van-detail-container">
+        {
+          <div className="van-detail">
+            <Link
+              to={`/vans-v2${currentVanSearch}`}
+              className="van-detail-v2-back-button"
+            >
+              &larr; <span>{`Back to ${currentVanType} vans`}</span>
+            </Link>
+            <img alt={vanDetail.name} src={vanDetail.imageUrl} />
+            <div
+              className={`van-type ${modifiedToClassCssName(vanDetail.type)} selected`}
+            >
+              {vanDetail.type}
+            </div>
+            <h2>{vanDetail.name}</h2>
+            <p className="van-price">
+              <span>${vanDetail.price}</span>/day
+            </p>
+            <p>{vanDetail.descriptions}</p>
+            <button className="link-button">Rent this van</button>
+          </div>
+        }
+      </div>
+    );
+  };
 
   return (
-    <div className="van-detail-container">
-      {
-        <div className="van-detail">
-          <Link
-            to={`/vans-v2${currentVanSearch}`}
-            className="van-detail-v2-back-button"
-          >
-            &larr; <span>{`Back to ${currentVanType} vans`}</span>
-          </Link>
-          <img alt={vanDetail.name} src={vanDetail.imageUrl} />
-          <div
-            className={`van-type ${modifiedToClassCssName(vanDetail.type)} selected`}
-          >
-            {vanDetail.type}
-          </div>
-          <h2>{vanDetail.name}</h2>
-          <p className="van-price">
-            <span>${vanDetail.price}</span>/day
-          </p>
-          <p>{vanDetail.descriptions}</p>
-          <button className="link-button">Rent this van</button>
-        </div>
-      }
-    </div>
+    <Suspense fallback={<Loading loadingName="van detail" />}>
+      <Await resolve={vanDetailPromise.vanById}>{renderVanDetailSection}</Await>
+    </Suspense>
   );
 }
 
+const getVanDetailById = async (idOfVanDetail) => {
+  try {
+    const vanDetailResponses = await processGetVanDetailById(idOfVanDetail);
+
+    return vanDetailResponses[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const vanDetailLoaderV2 = async ({ params, request }) => {
-  const pathName = new URL(request.url).pathname
+  const pathName = new URL(request.url).pathname;
 
   const { vanId } = params;
 
-  const getVanDetailById = async (idOfVanDetail) => {
-    try {
-      const vanDetailResponses = await processGetVanDetailById(idOfVanDetail);
-
-      return vanDetailResponses[0];
-    } catch (error) {
-      throw error;
-    }
-  };
-
   await authUserCheck(pathName);
 
-  return getVanDetailById(vanId);
+  return defer({ vanById: getVanDetailById(vanId) });
 };
