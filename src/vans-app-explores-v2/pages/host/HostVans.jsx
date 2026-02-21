0feaacff5e623ce91e31a7b7b12b9a8e2115763b
agustinusadam49 <agustinusadamw@gmail.com>
2026-02-21 09:04:22 +0700

    van-app-v2: implement defer, Await and suspense component, also loading component in host van list page

--- src/vans-app-explores-v2/pages/host/HostVans.jsx
+++ src/vans-app-explores-v2/pages/host/HostVans.jsx
@@ -1,54 +1,62 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import { processGetVanHostVans } from "../../api-calls-simulations/api-calls";
 import { authUserCheck } from "../../utils/index";
+import Loading from "../../components/Loading";
 
 export default function HostVans() {
-  const hostVans = useLoaderData();
+  const hostVansPromise = useLoaderData();
+
+  const renderHostVanListSection = (hostVans) => {
+    return (
+      <div className="host-vans-list">
+        <section>
+          {hostVans.map((hostVan) => (
+            <Link
+              to={`/host-v2/vans/${hostVan.id}`}
+              key={hostVan.id}
+              className="host-vans-link-wrapper"
+            >
+              <div className="host-vans-single" key={hostVan.id}>
+                <img src={hostVan.imageUrl} alt={hostVan.name} />
+                <div className="host-vans-info">
+                  <h3>{hostVan.name}</h3>
+                  <p>${hostVan.price}/day</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </section>
+      </div>
+    );
+  };
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
 
-      <div className="host-vans-list">
-        {hostVans.length ? (
-          <section>
-            {hostVans.map((hostVan) => (
-              <Link
-                to={`/host-v2/vans/${hostVan.id}`}
-                key={hostVan.id}
-                className="host-vans-link-wrapper"
-              >
-                <div className="host-vans-single" key={hostVan.id}>
-                  <img src={hostVan.imageUrl} alt={hostVan.name} />
-                  <div className="host-vans-info">
-                    <h3>{hostVan.name}</h3>
-                    <p>${hostVan.price}/day</p>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </section>
-        ) : (
-          <h2>Loading ...</h2>
-        )}
-      </div>
+      <Suspense fallback={<Loading loadingName="host vans" />}>
+        <Await resolve={hostVansPromise.hostVanListData}>
+          {renderHostVanListSection}
+        </Await>
+      </Suspense>
     </section>
   );
 }
 
-export const hostVansLoaderV2 = async ({request}) => {
-  const pathName = new URL(request.url).pathname;
+const getHostVans = async () => {
+  try {
+    const hostVansResponses = await processGetVanHostVans("123");
+    return hostVansResponses;
+  } catch (error) {
+    throw error;
+  }
+};
 
-  const getHostVans = async () => {
-    try {
-      const hostVansResponses = await processGetVanHostVans("123");
-      return hostVansResponses;
-    } catch (error) {
-      throw error;
-    }
-  };
+export const hostVansLoaderV2 = async ({ request }) => {
+  const pathName = new URL(request.url).pathname;
 
   await authUserCheck(pathName);
 
-  return getHostVans();
+  return defer({ hostVanListData: getHostVans() });
 };
