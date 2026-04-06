69c4432c7d0eb39500ea16d965d6c71b0bda2dc4
agustinusadam49 <agustinusadamw@gmail.com>
2026-04-06 19:52:54 +0700

    van-app-v1: change diare and ispa code disease code with the new codes

--- src/vans-app-explores/pages/home/show-case/read-and-write-multiple-files-simulation/constants.js
+++ src/vans-app-explores/pages/home/show-case/read-and-write-multiple-files-simulation/constants.js
@@ -1,31 +1,75 @@
+// OLD DIARE CODE DISEASE
+// export const DIARE_LISTS = [
+//   {
+//     disease: "A09",
+//     description: "Diarrhoea and gastroenteritis of presumed infectious origin",
+//     isAntibiotic: true,
+//   },
+//   {
+//     disease: "A09.9",
+//     description: "Gastroenteritis and colitis of unspecified origin",
+//     isAntibiotic: false,
+//   },
+//   {
+//     disease: "K52",
+//     description: "Other noninfective gastroenteritis and colitis",
+//     isAntibiotic: false,
+//   },
+// ];
+
+// OLD ISPA CODE DISEASE
+// export const ISPA_LISTS = [
+//   {
+//     disease: "J02",
+//     description: "Acute pharyngitis",
+//     isAntibiotic: true,
+//   },
+//   {
+//     disease: "J03",
+//     description: "Acute tonsillitis",
+//     isAntibiotic: true,
+//   },
+//   {
+//     disease: "J06",
+//     description:
+//       "Acute upper respiratory infections of multiple and unspecified sites",
+//     isAntibiotic: false,
+//   },
+//   {
+//     disease: "J18.0",
+//     description: "Bronchopneumonia, unspecified",
+//     isAntibiotic: true,
+//   },
+//   {
+//     disease: "R05",
+//     description: "Cough",
+//     isAntibiotic: true,
+//   },
+//   {
+//     disease: "J00",
+//     description: "Acute nasopharyngitis [common cold] ",
+//     isAntibiotic: false,
+//   },
+//   {
+//     disease: "J06.9",
+//     description: "Acute upper respiratory infection, unspecified",
+//     isAntibiotic: false,
+//   },
+// ];
+
 export const DIARE_LISTS = [
   {
     disease: "A09",
     description: "Diarrhoea and gastroenteritis of presumed infectious origin",
     isAntibiotic: true,
   },
-  {
-    disease: "A09.9",
-    description: "Gastroenteritis and colitis of unspecified origin",
-    isAntibiotic: false,
-  },
-  {
-    disease: "K52",
-    description: "Other noninfective gastroenteritis and colitis",
-    isAntibiotic: false,
-  },
 ];
 
 export const ISPA_LISTS = [
   {
-    disease: "J02",
-    description: "Acute pharyngitis",
-    isAntibiotic: true,
-  },
-  {
-    disease: "J03",
-    description: "Acute tonsillitis",
-    isAntibiotic: true,
+    disease: "J00",
+    description: "Acute nasopharyngitis [common cold] ",
+    isAntibiotic: false,
   },
   {
     disease: "J06",
@@ -34,23 +78,13 @@ export const ISPA_LISTS = [
     isAntibiotic: false,
   },
   {
-    disease: "J18.0",
-    description: "Bronchopneumonia, unspecified",
-    isAntibiotic: true,
-  },
-  {
-    disease: "R05",
-    description: "Cough",
-    isAntibiotic: true,
-  },
-  {
-    disease: "J00",
-    description: "Acute nasopharyngitis [common cold] ",
+    disease: "J09",
+    description: "influenza due to identified avian influenza virus",
     isAntibiotic: false,
   },
   {
-    disease: "J06.9",
-    description: "Acute upper respiratory infection, unspecified",
+    disease: "J11",
+    description: "influenza, virus not identified",
     isAntibiotic: false,
   },
 ];
